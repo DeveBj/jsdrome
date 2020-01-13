cd99533b95068545794c5aed0fe61259e6227a7f
m.balajivaishnav@gmail.com <DEVbj@1991>
2020-01-13 23:11:09 +0530

    feat: modifeid dev env file

--- _webpack/webpack.dev.js
+++ _webpack/webpack.dev.js
@@ -1,28 +1,7 @@
-const path = require('path');
+// const path = require('path');
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
 });
-module.exports = {
-  entry: {
-    app: './src/client/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, '../_dist'),
-    filename: 'app.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  mode: 'development',
-};
